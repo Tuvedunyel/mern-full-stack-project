4a2e0fcd5f0319cefc9fae1ce6b3c62b0579b48e
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2021-07-10 09:58:13 +0200

    User controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
+//auth
 router.post("/register", authController.signUp);
+
+// User DB
+router.get("/", userController.getAllUSers);
 
 module.exports = router;
